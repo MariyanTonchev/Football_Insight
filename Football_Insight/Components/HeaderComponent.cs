944bfb2cc018a0ca3784a3a4ed2a1bc5d420f37d
Mariyan Tonchev <mariyantonchev14@gmail.com>
2024-03-09 09:47:06 +0200

    Components

---
+++ Football_Insight/Components/HeaderComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Football_Insight.Components
+{
+    public class HeaderComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult<IViewComponentResult>(View());
+        }
+    }
+}
---
+++ Football_Insight/Components/MenuComponent.cs
@@ -0,0 +1,30 @@
+﻿using Football_Insight.Data;
+using Football_Insight.Models.League;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Football_Insight.Components
+{
+    public class MenuComponent : ViewComponent
+    {
+        private readonly FootballInsightDbContext context;
+
+        public MenuComponent(FootballInsightDbContext Context)
+        {
+            context = Context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var leagues = await context.Leagues
+                .Select(l => new LeagueViewModel
+                {
+                    Id = l.Id,
+                    Name = l.Name,
+                })
+                .ToListAsync();
+
+            return await Task.FromResult<IViewComponentResult>(View(leagues));
+        }
+    }
+}
